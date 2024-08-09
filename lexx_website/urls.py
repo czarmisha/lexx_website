0083fd9f4fb266309192d9af2a90be8e673f8ad3
m.dergachyov <m.dergachyov@aigroup.uz>
2024-08-09 12:09:17 +0500

    add langs

--- lexx_website/urls.py
+++ lexx_website/urls.py
@@ -2,13 +2,15 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 
-urls = [
+urls = i18n_patterns(
     path(settings.ADMIN_BASE_URL, admin.site.urls),
+    # path('rosetta/', include('rosetta.urls')),
     path("", include("web.urls", namespace="web")),
-]
+)
 urlpatterns = urls + debug_toolbar_urls()
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
